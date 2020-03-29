4358f8907c21c746b14823386023c6f61734d5d0
kangting <kangting@daojia-inc.com>
2020-03-29 12:07:43 +0800

    为元素添加class

---
+++ class-add/index.js
@@ -0,0 +1,234 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(typeof self !== 'undefined' ? self : this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @module
+ * @name class-add
+ * @description 为元素添加class
+ * @author kt
+ * @since 2019-01-03
+ *
+ *
+ * @param  {HTMLElement} ele 当前元素
+ * @param  {String} cls className名
+ * @example
+ *  var addClass = require('@fe/utils/class-add');
+ *  或
+ *  import addClass from '@fe/utils/class-add';
+ *  addClass(document.getElementById('app'), 'demo123');
+ */
+
+const hasClass = __webpack_require__(1);
+
+module.exports = (ele, cls) => {
+    // 判断参数是否有值
+    if (ele && cls) {
+        if (!hasClass(ele, cls)) {
+            ele.className += ' ' + cls;
+        }
+    }
+};
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+(function webpackUniversalModuleDefinition(root, factory) {
+  if (true) module.exports = factory();else if (typeof define === 'function' && define.amd) define([], factory);else {
+    var a = factory();
+    for (var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+  }
+})(typeof self !== 'undefined' ? self : this, function () {
+  return (/******/function (modules) {
+      // webpackBootstrap
+      /******/ // The module cache
+      /******/var installedModules = {};
+      /******/
+      /******/ // The require function
+      /******/function __webpack_require__(moduleId) {
+        /******/
+        /******/ // Check if module is in cache
+        /******/if (installedModules[moduleId]) {
+          /******/return installedModules[moduleId].exports;
+          /******/
+        }
+        /******/ // Create a new module (and put it into the cache)
+        /******/var module = installedModules[moduleId] = {
+          /******/i: moduleId,
+          /******/l: false,
+          /******/exports: {}
+          /******/ };
+        /******/
+        /******/ // Execute the module function
+        /******/modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+        /******/
+        /******/ // Flag the module as loaded
+        /******/module.l = true;
+        /******/
+        /******/ // Return the exports of the module
+        /******/return module.exports;
+        /******/
+      }
+      /******/
+      /******/
+      /******/ // expose the modules object (__webpack_modules__)
+      /******/__webpack_require__.m = modules;
+      /******/
+      /******/ // expose the module cache
+      /******/__webpack_require__.c = installedModules;
+      /******/
+      /******/ // define getter function for harmony exports
+      /******/__webpack_require__.d = function (exports, name, getter) {
+        /******/if (!__webpack_require__.o(exports, name)) {
+          /******/Object.defineProperty(exports, name, {
+            /******/configurable: false,
+            /******/enumerable: true,
+            /******/get: getter
+            /******/ });
+          /******/
+        }
+        /******/
+      };
+      /******/
+      /******/ // getDefaultExport function for compatibility with non-harmony modules
+      /******/__webpack_require__.n = function (module) {
+        /******/var getter = module && module.__esModule ?
+        /******/function getDefault() {
+          return module['default'];
+        } :
+        /******/function getModuleExports() {
+          return module;
+        };
+        /******/__webpack_require__.d(getter, 'a', getter);
+        /******/return getter;
+        /******/
+      };
+      /******/
+      /******/ // Object.prototype.hasOwnProperty.call
+      /******/__webpack_require__.o = function (object, property) {
+        return Object.prototype.hasOwnProperty.call(object, property);
+      };
+      /******/
+      /******/ // __webpack_public_path__
+      /******/__webpack_require__.p = "/";
+      /******/
+      /******/ // Load entry module and return exports
+      /******/return __webpack_require__(__webpack_require__.s = 0);
+      /******/
+    }(
+    /************************************************************************/
+    /******/[
+    /* 0 */
+    /***/function (module, exports) {
+
+      /**
+       * @module
+       * @name class-has
+       * @description 判断元素是否有某个class
+       * @author kt
+       * @since 2019-01-03
+       *
+       *
+       * @param {HTMLElement} ele 当前元素
+       * @param {String} cls className名
+       * @returns {Boolean} 是否有当前的class
+       * @example
+       *  var hasClass = require('@fe/utils/class-has');
+       *  或
+       *  import hasClass from '@fe/utils/class-has';
+       *  console.log(hasClass(document.getElementById('demo),'demo123'));
+       *  ========>false
+       */
+      module.exports = (ele, cls) => {
+        // 判断参数是否有值
+        if (ele && cls) {
+          return new RegExp('(\\s|^)' + cls + '(\\s|$)').test(ele.className);
+        }
+        return false;
+      };
+
+      /***/
+    }]
+    /******/)
+  );
+});
+
+/***/ })
+/******/ ]);
+});
---
+++ class-add/src/index.js
@@ -0,0 +1,27 @@
+/**
+ * @module
+ * @name class-add
+ * @description 为元素添加class
+ * @author kt
+ * @since 2019-01-03
+ *
+ *
+ * @param  {HTMLElement} ele 当前元素
+ * @param  {String} cls className名
+ * @example
+ *  var addClass = require('@fe/utils/class-add');
+ *  或
+ *  import addClass from '@fe/utils/class-add';
+ *  addClass(document.getElementById('app'), 'demo123');
+ */
+
+const hasClass = require('../../class-has');
+
+module.exports = (ele, cls) => {
+    // 判断参数是否有值
+    if (ele && cls) {
+        if (!hasClass(ele, cls)) {
+            ele.className += ' ' + cls;
+        }
+    }
+};
