e48f4a673eb55e6e08c1efb5ba6dff805fde28a7
kangting <kangting@daojia-inc.com>
2020-03-29 12:09:49 +0800

    防抖

---
+++ debounce/index.js
@@ -0,0 +1,116 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(typeof self !== 'undefined' ? self : this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+/**
+ * @module
+ * @name debounce
+ * @description 防抖
+ * @author 余良&kt
+ * @since 2018-12-07
+ *
+ *
+ * @param {function} func 回调函数
+ * @param {number} wait 等待时间
+ * @param {boolean} immediate 是否立即执行
+ * @return {function} 函数
+ * @example
+ *  var debounce = require('@fe/utils/debounce');
+ *  或
+ *  import debounce from '@fe/utils/debounce';
+ *  var consoleFn = function(){console.log(1);};// 无防抖函数
+ *  var consoleDebounceFn = debounce(function(){console.log(1);},300,false);// 防抖函数
+ */
+module.exports = (func, wait, immediate) => {
+    let timeout;
+    return function (...args) {
+        const context = this;
+        const later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+/***/ })
+/******/ ]);
+});
---
+++ debounce/src/index.js
@@ -0,0 +1,33 @@
+/**
+ * @module
+ * @name debounce
+ * @description 防抖
+ * @author kt
+ * @since 2018-12-07
+ *
+ *
+ * @param {function} func 回调函数
+ * @param {number} wait 等待时间
+ * @param {boolean} immediate 是否立即执行
+ * @return {function} 函数
+ * @example
+ *  var debounce = require('@fe/utils/debounce');
+ *  或
+ *  import debounce from '@fe/utils/debounce';
+ *  var consoleFn = function(){console.log(1);};// 无防抖函数
+ *  var consoleDebounceFn = debounce(function(){console.log(1);},300,false);// 防抖函数
+ */
+module.exports = (func, wait, immediate) => {
+    let timeout;
+    return function(...args) {
+        const context = this;
+        const later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
