3534cac9e8461aec58efb74517ce2e11ecfa210f
kangting <kangting@daojia-inc.com>
2020-03-29 12:08:40 +0800

    为元素移除class

---
+++ class-remove/src/index.js
@@ -0,0 +1,27 @@
+/**
+ * @module
+ * @name class-remove
+ * @description 为元素移除class
+ * @author kt
+ * @since 2019-01-03
+ *
+ *
+ * @param  {HTMLElement} ele 当前元素
+ * @param  {String} cls className名
+ * @example
+ *  var removeClass = require('@fe/utils/class-remove');
+ *  或
+ *  import removeClass from '@fe/utils/class-remove';
+ *  removeClass(document.getElementById('demo),'demo123');
+ */
+const hasClass = require('../../class-has');
+
+module.exports = (ele, cls) => {
+    // 判断参数是否有值
+    if (ele && cls) {
+        if (hasClass(ele, cls)) {
+            const reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+            ele.className = ele.className.replace(reg, ' ');
+        }
+    }
+};
