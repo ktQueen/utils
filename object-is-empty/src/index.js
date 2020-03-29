8a3955b036711f030fd99a9178be78d41fcdfcfe
kangting <kangting@daojia-inc.com>
2020-03-29 12:15:46 +0800

    判断`obj`是否为空

---
+++ object-is-empty/src/index.js
@@ -0,0 +1,25 @@
+/**
+ * @module
+ * @name object-is-empty
+ * @description 判断`obj`是否为空
+ * @author kt
+ * @since 2019-01-03
+ *
+ *
+ * @param {Object} obj 对象
+ * @returns {Boolean} value 对象是否为空
+ * @example
+ *  var objectIsEmpty = require('@fe/utils/object-is-empty');
+ *  或
+ *  import objectIsEmpty from '@fe/utils/object-is-empty';
+ *  console.log(objectIsEmpty({});
+ *  ======》true
+ */
+
+const dataType = require('../../data-type');
+
+module.exports = (obj) => {
+    if (dataType(obj) !== 'object') return false;
+    if (Array.isArray(obj)) return obj.length <= 0;
+    else return !Object.keys(obj).length;
+};
