72622712b62ee600be6bc97d2b095b7cd92514e8
kangting <kangting@daojia-inc.com>
2020-03-29 12:16:44 +0800

    在字符串中插入新字符串

---
+++ string-insert/src/index.js
@@ -0,0 +1,28 @@
+/**
+ * @module
+ * @name string-insert
+ * @description 在字符串中插入新字符串
+ * @author kt
+ * @since 2019-01-03
+ *
+ *
+ * @param {string} soure 源字符
+ * @param {number} start 插入字符的位置
+ * @param {string} newStr 需要插入的字符
+ * @returns {string} 返回新生成的字符
+ * @example
+ *  var stringInsert = require('@fe/utils/string-insert');
+ *  或
+ *  import stringInsert from '@fe/utils/string-insert';
+ *  console.log(stringInsert('sadsd',2,'qqqq'));
+ *  ======》saqqqqdsd
+ */
+
+const dataType = require('../../data-type');
+
+module.exports = (soure, start, newStr) => {
+    if (dataType(soure) !== 'string') return;
+    if (dataType(start) !== 'number') return;
+    if (dataType(newStr) !== 'string') return;
+    return soure.slice(0, start) + newStr + soure.slice(start);
+};
