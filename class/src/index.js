8f600b6de24905dce8ebdd99dd37fff0231ada95
kangting <kangting@daojia-inc.com>
2020-03-29 12:07:11 +0800

    元素class操作

---
+++ class/src/index.js
@@ -0,0 +1,48 @@
+/**
+ * @module
+ * @name class
+ * @description 元素class操作
+ * @author kt
+ * @since 2019-01-21
+ */
+const hasClass = require('../../class-has');
+const addClass = require('../../class-add');
+const removeClass = require('../../class-remove');
+
+module.exports = {
+    /**
+     * @description 为元素添加class
+     * @param  {HTMLElement} ele 当前元素
+     * @param  {String} cls className名
+     * @example
+     *  var classObj = require('@fe/utils/class');
+     *  或
+     *  import classObj from '@fe/utils/class';
+     *  classObj.addClass(document.getElementById('app'), 'demo123');
+     */
+    addClass:addClass,
+    /**
+     * @description 判断元素是否有某个class
+     * @param {HTMLElement} ele 当前元素
+     * @param {String} cls className名
+     * @return {Boolean} 是否有当前的class
+     * @example
+     *  var classObj = require('@fe/utils/class');
+     *  或
+     *  import classObj from '@fe/utils/class';
+     *  console.log(classObj.hasClass(document.getElementById('demo),'demo123'));
+     *  ========>false
+     */
+    hasClass:hasClass,
+    /**
+     * @description 为元素移除class
+     * @param  {HTMLElement} ele 当前元素
+     * @param  {String} cls className名
+     * @example
+     *  var classObj = require('@fe/utils/class-remove');
+     *  或
+     *  import classObj from '@fe/utils/class-remove';
+     *  classObj.removeClass(document.getElementById('demo),'demo123');
+     */
+    removeClass:removeClass
+};
