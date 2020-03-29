5b39a9a00a88b4f324adb7e4d474e9def70f0f9e
kangting <kangting@daojia-inc.com>
2020-03-29 12:12:09 +0800

    获取中文的code编码

---
+++ get-charcode/src/index.js
@@ -0,0 +1,27 @@
+/**
+ * @module
+ * @name get-charcode
+ * @description 获取中文的code编码
+ * @author kt
+ * @since 2018-11-29
+ *
+ * @param {string} data -传入的中文字符
+ * @return {string}  num -获取对应的中文的code编码
+ * @example
+ *  var getCharcode = require('@fe/utils/get-charcode');
+ *  或
+ *  import getCharcode from '@fe/utils/get-charcode';
+ *  getCharcode('您好');
+ *  ========>49488
+ */
+
+const dataType = require('../../data-type');
+
+module.exports = (data) => {
+    if (dataType(data) !== 'string') return;
+    let num = 0;
+    for (let i = 0; i < data.length; i++) {
+        num += parseInt(data.charCodeAt(data[i])) || 6;
+    }
+    return num;
+};
